8ad6c6c06283f31cec939b544cf3f032d98ee6e0
Sky Maya <skymaya@gmail.com>
2025-02-16 05:02:55 -0500

    change status_code to Uint16 and add request_duration

--- docker/clickhouse/clickhouse-init.sql
+++ docker/clickhouse/clickhouse-init.sql
@@ -3,8 +3,9 @@ CREATE DATABASE IF NOT EXISTS uptime_monitor;
 CREATE TABLE IF NOT EXISTS uptime_monitor.uptime_data (
     timestamp DateTime,
     url String,
-    status_code UInt32,
+    status_code UInt16,
     status_text String
+    request_duration UInt16
 ) ENGINE = MergeTree()
 ORDER BY (url, timestamp)
 TTL timestamp + INTERVAL 30 DAY;
