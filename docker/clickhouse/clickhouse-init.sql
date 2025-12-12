0b5c66754ff789c0876d22f8c7dab81fa89136af
smayadev <cyborg@cyborg>
2025-12-12 08:35:40 -0500

    fix typo in clickhouse init script

--- docker/clickhouse/clickhouse-init.sql
+++ docker/clickhouse/clickhouse-init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS uptime_monitor.uptime_data (
     timestamp DateTime,
     url String,
     status_code UInt16,
-    status_text String
+    status_text String,
     request_duration UInt16
 ) ENGINE = MergeTree()
 ORDER BY (url, timestamp)
